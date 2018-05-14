e172370cbd066a2be3c4d6a99626357a35deabe4
Jason Felice <jason.m.felice@gmail.com>
2018-05-13 20:27:19 -0400

    Benchmark against cparinfer interface

--- cparinfer/benches/perf.rs
+++ cparinfer/benches/perf.rs
@@ -1,10 +1,12 @@
-#![cfg(feature = "unstable")]
 #![feature(test)]
 
 extern crate test;
-extern crate parinfer;
 extern crate cparinfer;
 
+#[macro_use]
+extern crate serde_json;
+
+use std::ffi::CString;
 use test::Bencher;
 
 const LONG_MAP_WITH_STRINGS : &str = include_str!("perf/long_map_with_strings");
@@ -12,88 +14,110 @@ const REALLY_LONG_FILE : &str = include_str!("perf/really_long_file");
 const REALLY_LONG_FILE_WITH_UNCLOSED_PAREN : &str = include_str!("perf/really_long_file_with_unclosed_paren");
 const REALLY_LONG_FILE_WITH_UNCLOSED_QUOTE : &str = include_str!("perf/really_long_file_with_unclosed_quote");
 
-fn default_options<'a>() -> parinfer::Options<'a> {
-    parinfer::Options {
-        cursor_line: None,
-        cursor_x: None,
-        changes: vec![],
-        force_balance: false,
-        partial_result: false,
-        prev_cursor_x: None,
-        prev_cursor_line: None,
-        return_parens: false,
-        selection_start_line: None
-    }
+fn build_case(mode: &str, text: &str) -> CString {
+    CString::new(json!({
+        "mode": mode,
+        "text": text,
+        "options": {
+            "forceBalance": false,
+            "partialResult": false,
+            "returnParens": false
+        }
+    }).to_string()).unwrap()
 }
 
 #[bench]
 fn bench_paren_long_map_with_strings(b: &mut Bencher) {
-    let options = default_options();
-    b.iter(|| parinfer::paren_mode(LONG_MAP_WITH_STRINGS, &options));
+    unsafe {
+        let options = build_case("paren", LONG_MAP_WITH_STRINGS);
+        b.iter(|| cparinfer::run_parinfer(options.as_ptr()));
+    }
 }
 
 #[bench]
 fn bench_indent_long_map_with_strings(b: &mut Bencher) {
-    let options = default_options();
-    b.iter(|| parinfer::indent_mode(LONG_MAP_WITH_STRINGS, &options));
+    unsafe {
+        let options = build_case("indent", LONG_MAP_WITH_STRINGS);
+        b.iter(|| cparinfer::run_parinfer(options.as_ptr()));
+    }
 }
 
 #[bench]
 fn bench_smart_long_map_with_strings(b: &mut Bencher) {
-   let options = default_options();
-   b.iter(|| parinfer::smart_mode(LONG_MAP_WITH_STRINGS, &options));
+    unsafe {
+        let options = build_case("smart", LONG_MAP_WITH_STRINGS);
+        b.iter(|| cparinfer::run_parinfer(options.as_ptr()));
+    }
 }
 
 #[bench]
 fn bench_paren_really_long_file(b: &mut Bencher) {
-    let options = default_options();
-    b.iter(|| parinfer::paren_mode(REALLY_LONG_FILE, &options));
+    unsafe {
+        let options = build_case("paren", REALLY_LONG_FILE);
+        b.iter(|| cparinfer::run_parinfer(options.as_ptr()));
+    }
 }
 
 #[bench]
 fn bench_indent_really_long_file(b: &mut Bencher) {
-    let options = default_options();
-    b.iter(|| parinfer::indent_mode(REALLY_LONG_FILE, &options));
+    unsafe {
+        let options = build_case("indent", REALLY_LONG_FILE);
+        b.iter(|| cparinfer::run_parinfer(options.as_ptr()));
+    }
 }
 
 #[bench]
 fn bench_smart_really_long_file(b: &mut Bencher) {
-   let options = default_options();
-   b.iter(|| parinfer::smart_mode(REALLY_LONG_FILE, &options));
+    unsafe {
+        let options = build_case("smart", REALLY_LONG_FILE);
+        b.iter(|| cparinfer::run_parinfer(options.as_ptr()));
+    }
 }
 
 #[bench]
 fn bench_paren_really_long_file_with_unclosed_paren(b: &mut Bencher) {
-    let options = default_options();
-    b.iter(|| parinfer::paren_mode(REALLY_LONG_FILE_WITH_UNCLOSED_PAREN, &options));
+    unsafe {
+        let options = build_case("paren", REALLY_LONG_FILE_WITH_UNCLOSED_PAREN);
+        b.iter(|| cparinfer::run_parinfer(options.as_ptr()));
+    }
 }
 
 #[bench]
 fn bench_indent_really_long_file_with_unclosed_paren(b: &mut Bencher) {
-    let options = default_options();
-    b.iter(|| parinfer::indent_mode(REALLY_LONG_FILE_WITH_UNCLOSED_PAREN, &options));
+    unsafe {
+        let options = build_case("indent", REALLY_LONG_FILE_WITH_UNCLOSED_PAREN);
+        b.iter(|| cparinfer::run_parinfer(options.as_ptr()));
+    }
 }
 
 #[bench]
 fn bench_smart_really_long_file_with_unclosed_paren(b: &mut Bencher) {
-   let options = default_options();
-   b.iter(|| parinfer::smart_mode(REALLY_LONG_FILE_WITH_UNCLOSED_PAREN, &options));
+    unsafe {
+        let options = build_case("smart", REALLY_LONG_FILE_WITH_UNCLOSED_PAREN);
+        b.iter(|| cparinfer::run_parinfer(options.as_ptr()));
+    }
 }
 
 #[bench]
 fn bench_quote_really_long_file_with_unclosed_quote(b: &mut Bencher) {
-    let options = default_options();
-    b.iter(|| parinfer::paren_mode(REALLY_LONG_FILE_WITH_UNCLOSED_QUOTE, &options));
+    unsafe {
+        let options = build_case("paren", REALLY_LONG_FILE_WITH_UNCLOSED_QUOTE);
+        b.iter(|| cparinfer::run_parinfer(options.as_ptr()));
+    }
 }
 
 #[bench]
 fn bench_indent_really_long_file_with_unclosed_quote(b: &mut Bencher) {
-    let options = default_options();
-    b.iter(|| parinfer::indent_mode(REALLY_LONG_FILE_WITH_UNCLOSED_QUOTE, &options));
+    unsafe {
+        let options = build_case("indent", REALLY_LONG_FILE_WITH_UNCLOSED_QUOTE);
+        b.iter(|| cparinfer::run_parinfer(options.as_ptr()));
+    }
 }
 
 #[bench]
 fn bench_smart_really_long_file_with_unclosed_quote(b: &mut Bencher) {
-   let options = default_options();
-   b.iter(|| parinfer::smart_mode(REALLY_LONG_FILE_WITH_UNCLOSED_QUOTE, &options));
+    unsafe {
+        let options = build_case("smart", REALLY_LONG_FILE_WITH_UNCLOSED_QUOTE);
+        b.iter(|| cparinfer::run_parinfer(options.as_ptr()));
+    }
 }
