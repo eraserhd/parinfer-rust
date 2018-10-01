76741bd29cbd333293770d5042c5e601d50ed846
Jason Felice <jason.m.felice@gmail.com>
2018-10-01 12:13:18 +0100

    Move option parsing to cli_options

---
+++ src/cli_options.rs
@@ -0,0 +1,105 @@
+use getopts;
+use std::io;
+use std::io::Read;
+use serde_json;
+use types;
+use types::*;
+
+pub enum InputType {
+    Json,
+    Text
+}
+
+pub enum OutputType {
+    Json,
+    Text
+}
+
+pub struct Options {
+    matches: getopts::Matches
+}
+
+fn options() -> getopts::Options {
+    let mut options = getopts::Options::new();
+    options.optflag("h", "help", "show this help message");
+    options.optopt("", "input-format", "'json', 'text' (default: 'text')", "FMT");
+    options.optopt("m", "mode", "parinfer mode (indent, paren, or smart) (default: smart)", "MODE");
+    options.optopt("", "output-format", "'json', 'text' (default: 'text')", "FMT");
+    options
+}
+
+pub fn usage() -> String {
+    options().usage("Usage: parinfer-rust [options]")
+}
+
+impl Options {
+    pub fn parse(args: &[String]) -> Result<Options, String> {
+        options()
+            .parse(args)
+            .map(|m| Options {matches: m})
+            .map_err(|e| e.to_string())
+    }
+
+    pub fn want_help(&self) -> bool {
+        self.matches.opt_present("h")
+    }
+
+    fn mode(&self) -> &'static str {
+        match self.matches.opt_str("m") {
+            None => "smart",
+            Some(ref s) if s == "i" || s == "indent" => "indent",
+            Some(ref s) if s == "p" || s == "paren"  => "paren",
+            Some(ref s) if s == "s" || s == "smart"  => "smart",
+            _ => panic!("invalid mode specified for `-m`")
+        }
+    }
+
+    fn input_type(&self) -> InputType {
+        match self.matches.opt_str("input-format") {
+            None => InputType::Text,
+            Some(ref s) if s == "text" => InputType::Text,
+            Some(ref s) if s == "json" => InputType::Json,
+            Some(ref s) => panic!("unknown input format `{}`", s)
+        }
+    }
+
+    pub fn output_type(&self) -> OutputType {
+        match self.matches.opt_str("output-format") {
+            None => OutputType::Text,
+            Some(ref s) if s == "text" => OutputType::Text,
+            Some(ref s) if s == "json" => OutputType::Json,
+            Some(ref s) => panic!("unknown output fomrat `{}`", s)
+        }
+    }
+
+    pub fn request(&self) -> io::Result<Request> {
+        match self.input_type() {
+            InputType::Text => {
+                let mut text = String::new();
+                io::stdin().read_to_string(&mut text)?;
+                Ok(Request {
+                    mode: String::from(self.mode()),
+                    text,
+                    options: types::Options {
+                        changes: vec![],
+                        cursor_x: None,
+                        cursor_line: None,
+                        prev_text: None,
+                        prev_cursor_x: None,
+                        prev_cursor_line: None,
+                        force_balance: false,
+                        return_parens: false,
+                        partial_result: false,
+                        selection_start_line: None
+                    }
+                })
+            },
+            InputType::Json => {
+                let mut input = String::new();
+                io::stdin().read_to_string(&mut input)?;
+                Ok(serde_json::from_str(&input)?)
+            },
+        }
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -11,107 +11,29 @@ extern crate unicode_width;
 mod parinfer;
 mod types;
 mod changes;
+mod cli_options;
 
 use std::env;
 use std::io;
-use std::io::{Read,Write};
-use types::*;
+use std::io::Write;
+use cli_options::OutputType;
 
 extern crate getopts;
 
-enum InputType {
-    Json,
-    Text
-}
-
-enum OutputType {
-    Json,
-    Text
-}
-
-fn options() -> getopts::Options {
-    let mut options = getopts::Options::new();
-    options.optflag("h", "help", "show this help message");
-    options.optopt("", "input-format", "'json', 'text' (default: 'text')", "FMT");
-    options.optopt("m", "mode", "parinfer mode (indent, paren, or smart) (default: smart)", "MODE");
-    options.optopt("", "output-format", "'json', 'text' (default: 'text')", "FMT");
-    options
-}
-
-fn parse_args() -> getopts::Matches {
+fn parse_args() -> cli_options::Options {
     let args: Vec<String> = env::args().collect();
-    match options().parse(&args[1..]) {
-        Ok(matches) => matches,
-        Err(f) => { panic!(f.to_string()); }
-    }
-}
-
-fn mode(matches: &getopts::Matches) -> &'static str {
-    match matches.opt_str("m") {
-        None => "smart",
-        Some(ref s) if s == "i" || s == "indent" => "indent",
-        Some(ref s) if s == "p" || s == "paren"  => "paren",
-        Some(ref s) if s == "s" || s == "smart"  => "smart",
-        _ => panic!("invalid mode specified for `-m`")
-    }
-}
-
-fn input_type(matches: &getopts::Matches) -> InputType {
-    match matches.opt_str("input-format") {
-        None => InputType::Text,
-        Some(ref s) if s == "text" => InputType::Text,
-        Some(ref s) if s == "json" => InputType::Json,
-        Some(ref s) => panic!("unknown input format `{}`", s)
-    }
-}
-
-fn output_type(matches: &getopts::Matches) -> OutputType {
-    match matches.opt_str("output-format") {
-        None => OutputType::Text,
-        Some(ref s) if s == "text" => OutputType::Text,
-        Some(ref s) if s == "json" => OutputType::Json,
-        Some(ref s) => panic!("unknown output fomrat `{}`", s)
-    }
-}
-
-fn request(matches: &getopts::Matches) -> io::Result<Request> {
-    match input_type(matches) {
-        InputType::Text => {
-            let mut text = String::new();
-            io::stdin().read_to_string(&mut text)?;
-            Ok(Request {
-                mode: String::from(mode(&matches)),
-                text,
-                options: Options {
-                    changes: vec![],
-                    cursor_x: None,
-                    cursor_line: None,
-                    prev_text: None,
-                    prev_cursor_x: None,
-                    prev_cursor_line: None,
-                    force_balance: false,
-                    return_parens: false,
-                    partial_result: false,
-                    selection_start_line: None
-                }
-            })
-        },
-        InputType::Json => {
-            let mut input = String::new();
-            io::stdin().read_to_string(&mut input)?;
-            Ok(serde_json::from_str(&input)?)
-        },
-    }
+    cli_options::Options::parse(&args[1..])
+        .expect("failed to parse options")
 }
 
 pub fn main() {
-    let matches = parse_args();
-    if matches.opt_present("h") {
-        print!("{}", options().usage("Usage: parinfer-rust [options]"));
+    let opts = parse_args();
+    if opts.want_help() {
+        print!("{}", cli_options::usage());
     } else {
-        let request = request(&matches).expect("unable to parse options");
+        let request = opts.request().expect("unable to parse options");
         let answer = parinfer::process(&request);
-        let output = match output_type(&matches) {
+        let output = match opts.output_type() {
             OutputType::Json => serde_json::to_string(&answer).expect("unable to produce JSON"),
             OutputType::Text => String::from(answer.text)
         };
