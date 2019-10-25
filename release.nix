9aa044f2df2fc829f936c0003b525b7caf6416bd
Jason Felice <jason.m.felice@gmail.com>
2019-10-24 23:08:03 -0400

    Check Vim tests on CI

--- release.nix
+++ release.nix
@@ -3,9 +3,23 @@ let
   pkgs = import nixpkgs { config = {}; };
   parinfer-rust = pkgs.callPackage ./derivation.nix {};
 in {
-  test = parinfer-rust.overrideAttrs (self: {
+  unit-tests = parinfer-rust.overrideAttrs (self: {
     postBuild = ''
       RUST_BACKTRACE=1 cargo test
     '';
   });
+
+  vim-tests = pkgs.stdenv.mkDerivation {
+    name = "parinfer-rust-vim-tests-2019.10.24";
+    src = ./.;
+    buildPhase = ''
+      export VIM_TO_TEST=${pkgs.vim}/bin/vim
+      LC_ALL=en_US.UTF-8 \
+        LOCALE_ARCHIVE=${pkgs.glibcLocales}/lib/locale/locale-archive \
+        $VIM_TO_TEST --clean -u tests/run.vim
+    '';
+    installPhase = ''
+      touch $out
+    '';
+  };
 }
