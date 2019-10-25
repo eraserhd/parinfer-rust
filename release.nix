c1650acd09a18f63e2d0ff175b4e913d44357f31
Jason Felice <jason.m.felice@gmail.com>
2019-10-25 14:09:05 -0400

    Run tests on Neovim

--- release.nix
+++ release.nix
@@ -23,4 +23,19 @@ in {
       touch $out
     '';
   };
+
+  neovim-tests = pkgs.stdenv.mkDerivation {
+    name = "parinfer-rust-neovim-tests";
+    src = ./tests/vim;
+    buildPhase = ''
+      LC_ALL=en_US.UTF-8 \
+        LOCALE_ARCHIVE=${pkgs.glibcLocales}/lib/locale/locale-archive \
+        VIM_TO_TEST=${pkgs.neovim}/bin/nvim \
+        PLUGIN_TO_TEST=${parinfer-rust}/share/vim-plugins/parinfer-rust \
+        ${pkgs.vim}/bin/vim --clean -u run.vim
+    '';
+    installPhase = ''
+      touch $out
+    '';
+  };
 }
