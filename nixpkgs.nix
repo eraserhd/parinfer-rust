45276e00d5f883ca3918a22370b93e2373f86bfa
Jason Felice <jason.m.felice@gmail.com>
2021-03-19 15:19:58 -0400

    Bump nixpkgs

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,4 +1,4 @@
 builtins.fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/19.09-beta.tar.gz";
-  sha256 = "0fplfm2zx4vk7gs8bdcxnvzkdmpx2w0llqwf8475z9dz9cl132rm";
+  url = "https://github.com/NixOS/nixpkgs/archive/20.09.tar.gz";
+  sha256 = "1wg61h4gndm3vcprdcg7rc4s1v3jkm5xd7lw8r2f67w502y94gcy";
 }
