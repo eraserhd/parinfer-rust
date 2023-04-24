e28c86b5e75a33d35fa33740af01196da5a37362
Jason Felice <jason.m.felice@gmail.com>
2023-04-24 10:48:01 -0400

    Bump nixpkgs for Apple M1
    Closes #120

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,4 +1,4 @@
 builtins.fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/10f447c642b55dea64e36a27b037d6daa6f9f82f.tar.gz";
-  sha256 = "10jls9sas11p87hkz598h2k1ri3z5jrw5jja1pplslpjgi0j95kw";
+  url = "https://github.com/nixos/nixpkgs/archive/21.11.tar.gz";
+  sha256 = "162dywda2dvfj1248afxc45kcrg83appjd0nmdb541hl7rnncf02";
 }
